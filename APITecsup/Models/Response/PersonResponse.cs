d738640342e7773b592dca2d90115b6e9a5cca66
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2022-06-14 19:43:10 -0500

    Add project files.

---
+++ APITecsup/Models/Response/PersonResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APITecsup.Models.Response
+{
+    public class PersonResponse
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
